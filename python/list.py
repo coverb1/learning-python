800a44a081f8eb5c291a3defbfc8d3a2383dfdc3
Mucyobruce <cboy85096@email.com>
2026-03-28 20:29:28 +0200

    changes

--- python/list.py
+++ python/list.py
@@ -23,22 +23,70 @@
 
 #shopping cart
 
-foods=[]
-prices=[]
-total=0
-food=''
-while food.lower()!='q':
- food=input(f'enter your food of the  ')
- if food!='q':
-   price=float(input(f'enter your price of the :$ '))
-   foods.append(food)
-   prices.append(price)
-for food in foods:
-  print(food)
-for price in prices:
-  total+=price
-  print(f'your total is :${total}')
+#foods=[]
+#prices=[]
+#total=0
+#food=''
+#while food.lower()!='q':
+ #food=input(f'enter your food of the  ')
+ #if food!='q':
+ #  price=float(input(f'enter your price of the :$ '))
+  # foods.append(food)
+  # prices.append(price)
+#for food in foods:
+  #print(food)
+#for price in prices:
+ # total+=price
+  #print(f'your total is :${total}')
  
-    
-    
+
+
+
+# categories=({'apple','orange','banana','coconut'},
+#          {'dodo','carrot','patatoes'},
+#              {'inka','inkoko','ihene'}  )
+# for categorie in categories:
+#     for food in categories:
+#         print(food, end='\n')
+#     print()
+
+# # print(categories[0][2])
+
+
+# Quiz
+questions=('how many element are in periodic table:?',
+'which animal lays  the largest leg?:',
+'what is the most abundant gas in Earths atmosphere?: ',
+'how many bones are in human  body:c',
+'which planet in the solar system is the hotest')
   
+options=(("A.166","B.177","C.188","D.119"),
+         ("A.Whale","B.crocodile","C.Elephant","D.Ostrich"),
+         ("A.Nitrogen","B.Oxygen","C.Carbon-dioxide","D.Hydogen"),
+         ("A.206","B.207","C.208","D.209"),
+         ("A.Mercury","B.Venus","C.Earth","D.Mars"),
+         )
+
+answers=('C',"D","A","A","B")
+guesses=[]
+score=0
+question_num=0
+
+for question in questions:
+    print('-------------')
+    print(question)
+    for option in options[question_num]:
+        print(option)
+    guess=input("enter (A,B,C,D): ").upper()
+    guesses.append(guess)
+    if guess==answers[question_num]:
+        score+=1
+        print('CORRECT')
+    else:
+        print("INCORECT")
+        print(f'{answers[question_num]} is the correct answer')
+    question_num+=1
+
+print('______results______________')
+score=score/len(question)*100
+print(f'you score is {score}')
