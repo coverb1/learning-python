fc6e9d8ab93aed228e71a8c4da5b6e227016d1df
Mucyobruce <cboy85096@email.com>
2026-03-28 22:45:49 +0200

    changes

--- python/list.py
+++ python/list.py
@@ -54,39 +54,45 @@
 
 
 # Quiz
-questions=('how many element are in periodic table:?',
-'which animal lays  the largest leg?:',
-'what is the most abundant gas in Earths atmosphere?: ',
-'how many bones are in human  body:c',
-'which planet in the solar system is the hotest')
+# questions=('how many element are in periodic table:?',
+# 'which animal lays  the largest leg?:',
+# 'what is the most abundant gas in Earths atmosphere?: ',
+# 'how many bones are in human  body:c',
+# 'which planet in the solar system is the hotest')
   
-options=(("A.166","B.177","C.188","D.119"),
-         ("A.Whale","B.crocodile","C.Elephant","D.Ostrich"),
-         ("A.Nitrogen","B.Oxygen","C.Carbon-dioxide","D.Hydogen"),
-         ("A.206","B.207","C.208","D.209"),
-         ("A.Mercury","B.Venus","C.Earth","D.Mars"),
-         )
+# options=(("A.166","B.177","C.188","D.119"),
+#          ("A.Whale","B.crocodile","C.Elephant","D.Ostrich"),
+#          ("A.Nitrogen","B.Oxygen","C.Carbon-dioxide","D.Hydogen"),
+#          ("A.206","B.207","C.208","D.209"),
+#          ("A.Mercury","B.Venus","C.Earth","D.Mars"),
+#          )
 
-answers=('C',"D","A","A","B")
-guesses=[]
-score=0
-question_num=0
+# answers=('C',"D","A","A","B")
+# guesses=[]
+# score=0
+# question_num=0
 
-for question in questions:
-    print('-------------')
-    print(question)
-    for option in options[question_num]:
-        print(option)
-    guess=input("enter (A,B,C,D): ").upper()
-    guesses.append(guess)
-    if guess==answers[question_num]:
-        score+=1
-        print('CORRECT')
-    else:
-        print("INCORECT")
-        print(f'{answers[question_num]} is the correct answer')
-    question_num+=1
+# for question in questions:
+#     print('-------------')
+#     print(question)
+#     for option in options[question_num]:
+#         print(option)
+#     guess=input("enter (A,B,C,D): ").upper()
+#     guesses.append(guess)
+#     if guess==answers[question_num]:
+#         score+=1
+#         print('CORRECT')
+#     else:
+#         print("INCORECT")
+#         print(f'{answers[question_num]} is the correct answer')
+#     question_num+=1
 
-print('______results______________')
-score=score/len(question)*100
-print(f'you score is {score}')
+# print('______results______________')
+# score=score/len(question)*100
+# print(f'you score is {score}')
+
+# dictonary a =a collection of  {key:value} pairs
+capital={'USA':'washington Dc','Rwanda':'Kigali'}
+capital.update({'Congo':'Kinshasa'})
+print(capital)
+# print(capital.get('USA'))
