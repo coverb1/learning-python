686073ecbacc78e360b09c0104d4c95d82ead9fb
Mucyobruce <cboy85096@email.com>
2026-03-28 12:27:28 +0200

    list

---
+++ python/list.py
@@ -0,0 +1,19 @@
+#collection in python
+#collection single value used to store many things
+ 
+#list=[]
+#set={}
+#tuple=()
+
+fruits=["apple","orange","banana","coconut"]
+# print(fruits[0:3])
+# for x in fruits:
+#     print(x)
+# fruits[0]="amanda"
+# print(fruits)
+# fruits.append('mucyo')
+# fruits.remove('apple')
+# fruits.insert(2,"mucyo")
+fruits.sort()
+fruits.reverse()
+print(fruits)
--- python/main.py
+++ python/main.py
@@ -154,6 +154,8 @@
 
 # print("It is time wake up!")
 
-for x in range(3):
- for y in range(1,10):
-    print(y,end=' ')
+# for x in range(3):
+#  for y in range(1,10):
+#     print(y,end='')
+#  print()
+
